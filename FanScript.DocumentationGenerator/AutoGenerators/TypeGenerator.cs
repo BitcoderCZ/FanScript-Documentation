4ee874aef2dc8a7a7062cff238b506c4980a4cb3
BitcoderCZ <82174496+BitcoderCZ@users.noreply.github.com>
2024-09-12 08:23:41 +0200

    Code cleanup

--- FanScript.DocumentationGenerator/AutoGenerators/TypeGenerator.cs
+++ FanScript.DocumentationGenerator/AutoGenerators/TypeGenerator.cs
@@ -1,11 +1,5 @@
-﻿using FanScript.Compiler;
-using FanScript.Compiler.Symbols;
+﻿using FanScript.Compiler.Symbols;
 using FanScript.DocumentationGenerator.Utils;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FanScript.DocumentationGenerator.AutoGenerators
 {
